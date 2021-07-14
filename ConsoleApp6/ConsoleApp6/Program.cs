0ee65e4db438e582602672150e342b17b01f6a71
Hvhudu <shtormbreker2@gmail.com>
2021-07-14 16:35:38 +0300

    Initial commit
    /,

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace Phone_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db = new SqliteConnection("Data Source=usersdata3.db;Mode=ReadOnly;"))
+            {
+                db.Open();
+                    .
+                SqliteCommand command = new SqliteCommand();
+                command.Connection = db;
+            }
+
+        }
+    }
+}
